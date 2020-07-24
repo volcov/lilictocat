253e8a3c5558f12429e423f80e5b71222eddf57c
volcov <volcov.bruno@gmail.com>
2020-07-24 02:09:44 -0300

    add coveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Lilictocat.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test]
     ]
   end
 
@@ -21,7 +23,8 @@ defmodule Lilictocat.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:tentacat, "~> 2.0"}
+      {:tentacat, "~> 2.0"},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 end
