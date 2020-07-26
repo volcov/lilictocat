6edce2f61ae8f09f214226b8b878705f86f2d970
volcov <volcov.bruno@gmail.com>
2020-07-25 23:23:26 -0300

    add test for whoami

--- test/lilictocat_test.exs
+++ test/lilictocat_test.exs
@@ -1,4 +1,16 @@
 defmodule LilictocatTest do
   use ExUnit.Case
   doctest Lilictocat
+
+  import Mox
+
+  setup :verify_on_exit!
+
+  describe "whoami/0" do
+    test "returns who am i" do
+      expect(Lilictocat.Github.APIMock, :get_profile, fn -> %{"name" => "Lord Windgrace"} end)
+
+      assert Lilictocat.whoami() == "Lord Windgrace"
+    end
+  end
 end
