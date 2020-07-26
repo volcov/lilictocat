f71b8d5310914e44d6d71d4442ce6e01493bc328
volcov <volcov.bruno@gmail.com>
2020-07-25 23:20:28 -0300

    add specific congif files

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,6 @@
 import Config
 
 config :lilictocat, Lilictocat.Github, github_access_token: System.get_env("LILICTOCAT_TOKEN")
+config :lilictocat, :github_api, Lilictocat.Github.API
+
+import_config "#{Mix.env()}.exs"
