8a9bfd5b964f1656a49b96eae2934692f09f39f2
volcov <volcov.bruno@gmail.com>
2020-07-28 00:59:56 -0300

    add news tests

--- lib/lilictocat/github/github_behaviour.ex
+++ lib/lilictocat/github/github_behaviour.ex
@@ -1,4 +1,6 @@
 defmodule Lilictocat.GithubBehaviour do
   @callback get_profile() :: map()
   @callback get_organizations() :: list()
+  @callback get_organization_repos(String.t()) :: list()
+  @callback get_open_pulls(String.t(), String.t()) :: list()
 end
--- test/lilictocat/github_test.exs
+++ test/lilictocat/github_test.exs
@@ -15,10 +15,62 @@ defmodule Lilictocat.GithubTest do
     end
   end
 
-  describe "get_organizations/0" do
+  describe "organizations/0" do
     test "returns a list of organizations" do
       expect(Lilictocat.Github.APIMock, :get_organizations, fn -> [%{login: "dominaria inc"}] end)
       assert Github.organizations() == ["dominaria inc"]
+    end
+  end
+
+  describe "organization_repos/0" do
+    test "return a list of repos" do
+      expect(Lilictocat.Github.APIMock, :get_organizations, fn -> [%{login: "dominaria inc"}] end)
+
+      expect(Lilictocat.Github.APIMock, :get_organization_repos, fn _name ->
+        [
+          %{owner: %{login: "dominaria inc"}, name: "zoombie"},
+          %{owner: %{login: "dominaria inc"}, name: "goblin"}
+        ]
+      end)
+
+      assert Github.organization_repos() == [
+               %{owner: "dominaria inc", name: "zoombie"},
+               %{owner: "dominaria inc", name: "goblin"}
+             ]
+    end
+  end
+
+  describe "open_pull_requests_of_organization/0" do
+    test "return a list of urls" do
+      expect(Lilictocat.Github.APIMock, :get_organizations, fn -> [%{login: "dominaria inc"}] end)
+
+      expect(Lilictocat.Github.APIMock, :get_organization_repos, fn _name ->
+        [
+          %{owner: %{login: "dominaria inc"}, name: "zoombie"},
+          %{owner: %{login: "dominaria inc"}, name: "goblin"}
+        ]
+      end)
+
+      expect(Lilictocat.Github.APIMock, :get_open_pulls, fn _owner, _name ->
+        [
+          %{url: "https://link_pr.com/1"},
+          %{url: "https://link_pr.com/2"}
+        ]
+      end)
+
+      expect(Lilictocat.Github.APIMock, :get_open_pulls, fn _owner, _name ->
+        [
+          %{url: "https://link_pr.com/3"},
+          %{url: "https://link_pr.com/4"}
+        ]
+      end)
+
+      assert Github.open_pull_requests_of_organization() == [
+               "https://link_pr.com/1",
+               "https://link_pr.com/2",
+               "https://link_pr.com/3",
+               "https://link_pr.com/4"
+             ]
     end
   end
 end
