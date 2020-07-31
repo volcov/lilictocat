ab1eb7c32ac4bc02eac7be26846164e3b5ba6a9e
volcov <volcov.bruno@gmail.com>
2020-07-31 00:56:37 -0300

    add function to return oldest pr

--- lib/lilictocat.ex
+++ lib/lilictocat.ex
@@ -8,4 +8,14 @@ defmodule Lilictocat do
   def whoami do
     Github.name()
   end
+
+  def my_open_pull_requests do
+    Github.open_pull_requests_of_organization()
+  end
+
+  def get_my_oldest_opened_pull_request do
+    my_open_pull_requests()
+    |> Enum.min_by(& &1.created_at, DateTime)
+    |> Map.get(:link)
+  end
 end
