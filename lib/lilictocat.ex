b2a8daf2a979ae2d0fc23af50c82e122468930f4
volcov <volcov.bruno@gmail.com>
2020-07-31 01:00:56 -0300

    remove unused function

--- lib/lilictocat.ex
+++ lib/lilictocat.ex
@@ -9,12 +9,8 @@ defmodule Lilictocat do
     Github.name()
   end
 
-  def my_open_pull_requests do
-    Github.open_pull_requests_of_organization()
-  end
-
   def get_my_oldest_opened_pull_request do
-    my_open_pull_requests()
+    Github.open_pull_requests_of_organization()
     |> Enum.min_by(& &1.created_at, DateTime)
     |> Map.get(:link)
   end
