3f3f5f4e19cdd820034b2e05839aea6f2bbdc5f8
volcov <volcov.bruno@gmail.com>
2020-08-02 22:23:49 -0300

    add function to get reviews

--- lib/lilictocat.ex
+++ lib/lilictocat.ex
@@ -14,4 +14,11 @@ defmodule Lilictocat do
     |> Enum.min_by(& &1.created_at, DateTime)
     |> Map.get(:link)
   end
+
+  def get_oldest_pull_request_without_review do
+    Github.open_pull_requests_of_organization()
+    |> Enum.filter(fn pr -> Github.pull_request_without_review?(pr) end)
+    |> Enum.min_by(& &1.created_at, DateTime)
+    |> Map.get(:link)
+  end
 end
