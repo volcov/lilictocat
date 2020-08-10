5c5c9b147e849d55d3493031781d8555820f8893
volcov <volcov.bruno@gmail.com>
2020-08-10 18:17:42 -0300

    add moduledoc

--- lib/lilictocat.ex
+++ lib/lilictocat.ex
@@ -2,9 +2,14 @@ defmodule Lilictocat do
   alias Lilictocat.Github
 
   @moduledoc """
-  coming soon
+    Lilictocat is a simple pull request "finder"
+
+    based on pre-defined rules by the user, it will look for the pull request link in your organization that best fits the rule
+
+    in this alpha version we are only working with one rule, in the future we will have a way to customize your own rules
   """
 
+  @spec get_oldest_pull_request_without_review() :: String.t()
   def get_oldest_pull_request_without_review do
     Github.open_pull_requests_of_organization()
     |> Stream.filter(fn pr -> Github.pull_request_without_review?(pr) end)
