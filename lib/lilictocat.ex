2cede51ff224c7abec4b410db38629b7ba3195eb
volcov <volcov.bruno@gmail.com>
2020-09-16 01:23:47 -0300

    pass arguments into function

--- lib/lilictocat.ex
+++ lib/lilictocat.ex
@@ -17,11 +17,14 @@ defmodule Lilictocat do
       iex> Lilictocat.get_oldest_pull_request_without_review()
       "https://github.com/dominaria/zoombie/pull/666"
 
+      iex> Lilictocat.get_oldest_pull_request_without_review()
+      "https://github.com/dominaria/goblin/pull/132"
+
   """
 
   @spec get_oldest_pull_request_without_review(list()) :: String.t()
   def get_oldest_pull_request_without_review(arguments \\ []) do
-    Github.open_pull_requests_of_organization()
+    Github.open_pull_requests_of_organization(arguments)
     |> Stream.filter(fn pr -> Github.pull_request_without_review?(pr) end)
     |> Enum.min_by(& &1.created_at, DateTime, fn -> %{link: "no pr's to list"} end)
     |> Map.get(:link)
